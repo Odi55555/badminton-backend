e12fb16ef6d2fa9a94c65e19b69585dcd51a8b79
Armin Odenkirchen <aowerb@gmail.com>
2015-12-15 19:25:59 +0100

    new "getRegistrations" method which returns registrations with player details for a specific game

--- common/models/registration.js
+++ common/models/registration.js
@@ -1,3 +1,31 @@
 module.exports = function(Registration) {
-
+	Registration.getRegistrations = function(gameId, callback) {
+		var app = Registration.app;
+		var Game = app.models.Game;
+		Game.findById(gameId, function(err, game) {
+			game.registrations({
+				include: 'player'
+			}, function(err, registrations) {
+				callback(null, registrations);
+			});
+		});
+	};
+	Registration.remoteMethod(
+		'getRegistrations', {
+			http: {
+				path: '/getRegistrations',
+				verb: 'post'
+			},
+			accepts: [{
+				arg: 'gameId',
+				type: 'string'
+			}],
+			descrption: 'TODO: describe me',
+			// TODO optimze return values
+			returns: {
+				arg: 'registrations',
+				type: 'Object'
+			}
+		}
+	);
 };
